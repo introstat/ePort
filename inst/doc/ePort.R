f6eff779a12938f8b6b21f356e868c9c21983164
Lindsay Rutter <lindsayannerutter@gmail.com>
2015-12-22 23:11:28 -0500

    Added topic graphics

--- inst/doc/ePort.R
+++ inst/doc/ePort.R
@@ -1,7 +1,7 @@
 ### R code from vignette source 'ePort.Rnw'
 
 ###################################################
-### code chunk number 1: ePort.Rnw:32-37
+### code chunk number 1: ePort.Rnw:35-40
 ###################################################
 library(knitr)
 opts_chunk$set(
@@ -18,44 +18,44 @@ options(SweaveHooks = list(fig = function() par(mar=c(3,3,1,0.5),mgp = c(2,1,0))
 
 
 ###################################################
-### code chunk number 3: ePort.Rnw:62-64
+### code chunk number 3: ePort.Rnw:65-67
 ###################################################
 rm(list=ls())
 library(knitr)
 
 
 ###################################################
-### code chunk number 4: ePort.Rnw:87-88 (eval = FALSE)
+### code chunk number 4: ePort.Rnw:90-91 (eval = FALSE)
 ###################################################
 ## install.packages("ePort")
 
 
 ###################################################
-### code chunk number 5: ePort.Rnw:94-95
+### code chunk number 5: ePort.Rnw:97-98
 ###################################################
 library(ePort)
 
 
 ###################################################
-### code chunk number 6: ePort.Rnw:102-103 (eval = FALSE)
+### code chunk number 6: ePort.Rnw:105-106 (eval = FALSE)
 ###################################################
 ## help(package="ePort")
 
 
 ###################################################
-### code chunk number 7: ePort.Rnw:108-109 (eval = FALSE)
+### code chunk number 7: ePort.Rnw:111-112 (eval = FALSE)
 ###################################################
 ## help(mergeSection)
 
 
 ###################################################
-### code chunk number 8: ePort.Rnw:130-131 (eval = FALSE)
+### code chunk number 8: ePort.Rnw:215-216 (eval = FALSE)
 ###################################################
 ## system.file("inst/extdata/", package="ePort")
 
 
 ###################################################
-### code chunk number 9: ePort.Rnw:165-180 (eval = FALSE)
+### code chunk number 9: ePort.Rnw:250-265 (eval = FALSE)
 ###################################################
 ## key_htm = system.file("inst/extdata/KeyFiles/Topic06.Questions.htm", package="ePort")
 ## 
@@ -75,68 +75,68 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 10: ePort.Rnw:185-186 (eval = FALSE)
+### code chunk number 10: ePort.Rnw:270-271 (eval = FALSE)
 ###################################################
 ## key_htm = system.file("inst/extdata/KeyFiles/Topic06.Questions.htm", package="ePort")
 
 
 ###################################################
-### code chunk number 11: ePort.Rnw:191-192 (eval = FALSE)
+### code chunk number 11: ePort.Rnw:276-277 (eval = FALSE)
 ###################################################
 ## refineKey(key_htm)
 
 
 ###################################################
-### code chunk number 12: ePort.Rnw:199-200 (eval = FALSE)
+### code chunk number 12: ePort.Rnw:284-285 (eval = FALSE)
 ###################################################
 ## keyPath = gsub("htm$","txt",key_htm)
 
 
 ###################################################
-### code chunk number 13: ePort.Rnw:205-206 (eval = FALSE)
+### code chunk number 13: ePort.Rnw:290-291 (eval = FALSE)
 ###################################################
 ## dataPath = system.file("inst/extdata/DataFiles/Topic06/Topic06.AB.csv", package="ePort")
 
 
 ###################################################
-### code chunk number 14: ePort.Rnw:211-212 (eval = FALSE)
+### code chunk number 14: ePort.Rnw:296-297 (eval = FALSE)
 ###################################################
 ## rewriteData(dataPath)
 
 
 ###################################################
-### code chunk number 15: ePort.Rnw:217-218 (eval = FALSE)
+### code chunk number 15: ePort.Rnw:302-303 (eval = FALSE)
 ###################################################
 ## loPath = system.file("inst/extdata/LOFiles/Topic06.Outcomes.txt", package="ePort")
 
 
 ###################################################
-### code chunk number 16: ePort.Rnw:223-224 (eval = FALSE)
+### code chunk number 16: ePort.Rnw:308-309 (eval = FALSE)
 ###################################################
 ## outPath = system.file("inst/extdata/OutputFiles", package="ePort")
 
 
 ###################################################
-### code chunk number 17: ePort.Rnw:229-230 (eval = FALSE)
+### code chunk number 17: ePort.Rnw:314-315 (eval = FALSE)
 ###################################################
 ## makeReport(keyFile=keyPath, dataFile=dataPath, loFile=loPath, outFile=outPath)
 
 
 ###################################################
-### code chunk number 18: ePort.Rnw:251-253 (eval = FALSE)
+### code chunk number 18: ePort.Rnw:336-338 (eval = FALSE)
 ###################################################
 ## makeReport(keyFile=keyPath, dataFile=dataPath, loFile=loPath, outFile=outPath,
 ## reportType = "secTopicShort")
 
 
 ###################################################
-### code chunk number 19: ePort.Rnw:267-268 (eval = FALSE)
+### code chunk number 19: ePort.Rnw:352-353 (eval = FALSE)
 ###################################################
 ## makeReport(keyFile = keyPath, dataFile = dataPath, loFile = loPath, outFile = outPath, reportType = "secTopicLong")
 
 
 ###################################################
-### code chunk number 20: ePort.Rnw:277-280 (eval = FALSE)
+### code chunk number 20: ePort.Rnw:362-365 (eval = FALSE)
 ###################################################
 ## dataFolder = system.file("inst/extdata/DataFiles/Topic06", package = "ePort")
 ## dataList = list.files(path = dataFolder, full.names = TRUE)[1:2]
@@ -144,13 +144,13 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 21: ePort.Rnw:287-288 (eval = FALSE)
+### code chunk number 21: ePort.Rnw:372-373 (eval = FALSE)
 ###################################################
 ## makeReport(keyFile = keyPath, dataFile = dataList, loFile = loPath, outFile = outPath, reportType = "crossSecTopicLong")
 
 
 ###################################################
-### code chunk number 22: ePort.Rnw:293-305 (eval = FALSE)
+### code chunk number 22: ePort.Rnw:378-390 (eval = FALSE)
 ###################################################
 ## dataFolder = system.file("inst/extdata/DataFiles/Topic03_06", package="ePort")
 ## dataList = list.files(path = dataFolder, full.names = TRUE)
@@ -167,14 +167,14 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 23: ePort.Rnw:310-312 (eval = FALSE)
+### code chunk number 23: ePort.Rnw:395-397 (eval = FALSE)
 ###################################################
 ## merged = subsetData(mergedData, dataTable)
 ## knit("/Users/lindz/ePort/inst/Rnw/hw-topic-section.Rnw", output = 'Stat101hwk_Unit1_allSections.tex')
 
 
 ###################################################
-### code chunk number 24: ePort.Rnw:326-332 (eval = FALSE)
+### code chunk number 24: ePort.Rnw:411-417 (eval = FALSE)
 ###################################################
 ## dataFolder = system.file("inst/extdata/DataFiles/Topic03_Split", package="ePort")
 ## dataList = list.files(path=dataFolder,full.names=TRUE)[1:2]
@@ -185,7 +185,7 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 25: ePort.Rnw:339-346 (eval = FALSE)
+### code chunk number 25: ePort.Rnw:424-431 (eval = FALSE)
 ###################################################
 ## dataPath = "~/Dropbox/NSF Grant 2013-2015/Semesters/Fall 2014/Data Files/Ch9-Ch11"
 ## namelist = list.files(path=dataPath,full.names=TRUE)
@@ -197,7 +197,7 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 26: ePort.Rnw:360-363 (eval = FALSE)
+### code chunk number 26: ePort.Rnw:445-448 (eval = FALSE)
 ###################################################
 ## dataFolder = system.file("inst/extdata/DataFiles/Topic03_Deidentified", package="ePort")
 ## getNameList(dataFolder, section=NULL, semester=NULL, secblind=TRUE, save=TRUE)
@@ -205,14 +205,14 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 27: ePort.Rnw:373-375 (eval = FALSE)
+### code chunk number 27: ePort.Rnw:458-460 (eval = FALSE)
 ###################################################
 ## dataListPath = c(system.file("inst/extdata/DataFiles/Topic06/Topic06.AB.csv", package="ePort"),
 ## system.file("inst/extdata/DataFiles/Topic06/Topic06.CD.csv", package="ePort"))
 
 
 ###################################################
-### code chunk number 28: ePort.Rnw:380-384 (eval = FALSE)
+### code chunk number 28: ePort.Rnw:465-469 (eval = FALSE)
 ###################################################
 ## for (i in dataListPath){
 ##   rewriteData(i)
@@ -221,7 +221,7 @@ library(ePort)
 
 
 ###################################################
-### code chunk number 29: ePort.Rnw:389-399 (eval = FALSE)
+### code chunk number 29: ePort.Rnw:474-484 (eval = FALSE)
 ###################################################
 ## dataFolder = system.file("inst/extdata/DataFiles/Topic06", package="ePort")
 ## #namelist = list.files(path=dataFolder, pattern = "^[^.]*\.[^.]*\.[^.]*$", full.names=FALSE)
