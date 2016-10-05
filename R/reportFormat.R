5fbc45211a226c7ceb27bc4501b11daeeff0cedc
Lindsay Rutter <lindsayannerutter@gmail.com>
2016-10-05 12:51:00 -0500

    use @importFrom in reportsFormat.R

--- R/reportFormat.R
+++ R/reportFormat.R
@@ -29,6 +29,9 @@ globalVariables(c("menu", "setNames", "knit2pdf", "read.delim"))
 #' @importFrom knitr knit
 #' @importFrom stringr str_trim
 #' @importFrom Matrix diag
+#' @importFrom fpc pamk
+#' @importFrom lme4 fixef
+#' @importFrom ggplot2 qplot
 #' @export
 #'
 makeReport = function(keyFile=NULL, dataFile=NULL, loFile=NULL, outFile=NULL, reportType = NULL, keepFiles=FALSE, keepTex=FALSE, keepImage=FALSE, className = "Stat101", lowScore = 80, repeatLowScore = 20, topic=NULL, section=NULL, path=NULL, type=NULL, unit = 1, rewrite=FALSE, skip=NULL){  
